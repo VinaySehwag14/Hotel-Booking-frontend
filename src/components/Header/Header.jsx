46e374da731a23e4bd5fc07145a64be197276b5b
VinaySehwag14 <vinaysehwag14@gmail.com>
2023-02-13 18:45:07 +0530

    added style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import {
   faBed,
+  faCalendarDays,
   faCar,
+  faPerson,
   faPlane,
   faTaxi,
 } from "@fortawesome/free-solid-svg-icons";
@@ -10,7 +12,7 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="flex bg-blue-900 text-white justify-center">
+    <div className="flex bg-blue-900 text-white justify-center relative">
       <div className="w-full max-w-5xl mt-5 mx-0 mb-24">
         <div className="flex gap-10 mb-12">
           <div className="flex gap-2.5 items-center active:outline-2  headBtn active">
@@ -44,6 +46,30 @@ const Header = () => {
         <button className="bg-blue-500 text-white font-bold p-2.5">
           Sign in / Register
         </button>
+        <div className="h-14 bg-white border-[5px] border-orange-300 flex items-center justify-around py-2.5 px-0 rounded absolute -bottom-7 w-full max-w-5xl">
+          {/* my-2.5  rounded  bottom-6 w-full max-w-5xl */}
+          <div className="flex items-center gap-2.5">
+            <FontAwesomeIcon icon={faBed} className="text-gray-600" />
+            <input
+              type="text"
+              placeholder="Where are you going?"
+              className="border-none outline-none text-black"
+            />
+          </div>
+          <div className="flex items-center gap-2.5">
+            <FontAwesomeIcon icon={faCalendarDays} className="text-gray-600" />
+            <span className="text-gray-600">Date to date</span>
+          </div>
+          <div className="flex items-center gap-2.5">
+            <FontAwesomeIcon icon={faPerson} className="text-gray-600" />
+            <span className="text-gray-600">2 aduls 2 childer</span>
+          </div>
+          <div className="flex items-center gap-2.5">
+            <button className="bg-blue-900 text-white font-medium p-2.5">
+              Search
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );

3f11088bd381601c515570ac84e923f95c51f60d
VinaySehwag14 <vinaysehwag14@gmail.com>
2023-03-03 13:49:21 +0530

    changes on login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -48,7 +48,7 @@ const Login = () => {
                   type="text"
                   className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                   id="username"
-                  placeholder="User Name"
+                  placeholder="enter malang"
                   onChange={handleChange}
                 />
               </div>
@@ -58,7 +58,7 @@ const Login = () => {
                   type="password"
                   className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                   id="password"
-                  placeholder="Password"
+                  placeholder="enter freewolf"
                   onChange={handleChange}
                 />
               </div>
